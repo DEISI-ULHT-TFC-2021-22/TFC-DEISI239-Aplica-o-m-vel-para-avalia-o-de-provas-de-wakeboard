8f9cc2bbd49dbee43c911c089d13f02c880807cd
matheusbralves <matheusbralvess@gmail.com>
2022-03-02 14:44:15 +0000

    Added images

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,12 +1,19 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, Text, StyleSheet, Button, Image } from 'react-native';
 
 import DefaultStyles from '../constants/default-styles';
 
 const GameOverScreen = props => {
     return (
         <View style={styles.screen}>
-            <Text style={DefaultStyles.bodyText}>The Game is Over!</Text>
+            <Text style={DefaultStyles.title}>The Game is Over!</Text>
+            <View style={styles.imageContainer}>
+                <Image 
+                    source={require('../assets/images/success.png')} 
+                    style={styles.image}
+                    resizeMode="cover"
+                />
+            </View>
             <Text style={DefaultStyles.bodyText}>Number of rounds: {props.roundsNumber}</Text>
             <Text style={DefaultStyles.bodyText}>Number was: {props.userNumber}</Text>
             <Button title="NEW GAME" onPress={props.onRestart} />
@@ -19,6 +26,21 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+    },
+
+    imageContainer:{
+        width: 300,
+        height: 300,
+        borderRadius: 150,
+        borderWidth: 3,
+        borderColor: 'black',
+        overflow: 'hidden',
+        marginVertical: 30,
+    },
+
+    image:{
+        width: '100%',
+        height: '100%',
     },
 });
 
