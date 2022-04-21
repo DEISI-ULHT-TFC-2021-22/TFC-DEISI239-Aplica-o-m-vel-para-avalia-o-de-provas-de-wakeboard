fec75a328b151ddc46c2ef33be125cacfc276b7e
matheusbralves <matheusbralvess@gmail.com>
2022-04-21 13:57:21 +0100

    Trick component first layer working

--- components/Trick.js
+++ components/Trick.js
@@ -1,20 +1,109 @@
-import React from 'react';
-import { View, StyleSheet, Button, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, StyleSheet, Button, FlatList } from 'react-native';
+import Rolls from '../constants/rolls'
+import Tantrums from '../constants/tantrums'
+import Raleys from '../constants/raley'
 
 const Trick = props => {
+
+    const [selectTrick, setSelectTrick] = useState(false);
+    const [textSelectTrick, setTextSelectTrick] = useState("");
+
+    const selectTrickHandler = (buttonText) => {
+        setSelectTrick(true);
+        setTextSelectTrick(buttonText);
+    }
+
+    const [selectTrick2, setSelectTrick2] = useState(false);
+    const [textSelectTrick2, setTextSelectTrick2] = useState("");
+
+    const selectTrickHandler2 = (buttonText) => {
+        setSelectTrick2(true);
+        setTextSelectTrick2(buttonText);
+    }
+    
+    const renderFirstLayer = () => {
+        if (selectTrick == true && textSelectTrick == "Rolls") {
+            return (
+                <FlatList
+                    keyExtractor={(item, index) => item.id}
+                    data={Rolls.layer1Rolls}
+                    renderItem={itemData => <Button 
+                        id={itemData.item.id} 
+                        title={itemData.item} 
+                        onPress={() => selectTrickHandler2(itemData.item)}
+                    />}
+                />
+            )
+        }
+
+        if (selectTrick == true && textSelectTrick == "Tantrums") {
+            return (
+                <FlatList
+                    keyExtractor={(item, index) => item.id}
+                    data={Tantrums.layer1Tantrums}
+                    renderItem={itemData => <Button id={itemData.item.id} title={itemData.item} />}
+                />
+            )
+        }
+
+        if (selectTrick == true && textSelectTrick == "Raleys") {
+            return (
+                <FlatList
+                    keyExtractor={(item, index) => item.id}
+                    data={Raleys.layer1Raley}
+                    renderItem={itemData => <Button id={itemData.item.id} title={itemData.item} />}
+                />
+            )
+        }
+    }
+
+    const renderSecondLayer = () => {
+        if (selectTrick2 == true && textSelectTrick2 == "HS BACKROLLS") {
+            return (
+                <FlatList
+                    keyExtractor={(item, index) => item.id}
+                    data={Rolls.layer2HS}
+                    renderItem={itemData => <Button id={itemData.item.id} title={itemData.item} />}
+                />
+            )
+        }
+    }
+
     return (
         <View style={styles.both}>
             <View style={styles.containerM}>
 
                 <View style={styles.choices}>
                     <View style={styles.itemMan}>
-                        <Button title="Rolls" />
+                        <Button
+                            title="Rolls"
+                            onPress={() => selectTrickHandler("Rolls")}
+                        />
                     </View>
                     <View style={styles.itemMan}>
-                        <Button title="Raleys" />
+                        <Button 
+                            title="Raleys" 
+                            onPress={() => selectTrickHandler("Raleys")}
+                        />
                     </View>
                     <View style={styles.itemMan}>
-                        <Button title="Tantrums" />
+                        <Button
+                            title="Tantrums" 
+                            onPress={() => selectTrickHandler("Tantrums")}
+                        />
+                    </View>
+                </View>
+
+                <View style={styles.choices}>
+                    <View style={styles.itemMan}>
+                        {renderFirstLayer()}
+                    </View>
+                </View>
+
+                <View style={styles.choices}>
+                    <View style={styles.itemMan}>
+                        {renderSecondLayer()}
                     </View>
                 </View>
 
